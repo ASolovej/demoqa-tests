948e8dcee8a8dbf2bea8a3da9ac6f54dc7a30101
Alexandr Solovyov <18solovjov@gmail.com>
2023-06-07 23:36:39 +0300

    add allureListener

--- src/test/java/BaseTest.java
+++ src/test/java/BaseTest.java
@@ -1,7 +1,10 @@
 import com.codeborne.selenide.Configuration;
 import com.codeborne.selenide.Selenide;
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.BeforeEach;
 
 public class BaseTest {
 
@@ -12,6 +15,11 @@ public class BaseTest {
         Configuration.pageLoadStrategy = "none";
         Configuration.baseUrl = "https://demoqa.com/automation-practice-form";
 //        Configuration.holdBrowserOpen = true;
+    }
+
+    @BeforeEach
+    public void addListener() {
+        SelenideLogger.addListener("Allure", new AllureSelenide());
     }
 
     @AfterAll
