9699521fa11aa30d1ba5b393690327fcfc3db2f0
Alexandr Solovyov <18solovjov@gmail.com>
2023-06-11 16:41:31 +0300

    add step in RegistrationPage

--- src/test/java/pages/RegistrationPage.java
+++ src/test/java/pages/RegistrationPage.java
@@ -28,32 +28,37 @@ public class RegistrationPage {
         calendarComponent = new CalendarComponent();
     }
 
-
+    @Step("Ввести в поле First Name {firstName}")
     public RegistrationPage setFirstName(String firstName) {
         $("#firstName").setValue(firstName);
         return this;
     }
 
+    @Step("Ввести в поле Last Name {lastName}")
     public RegistrationPage setLastName(String lastName) {
         $("#lastName").setValue(lastName);
         return this;
     }
 
+    @Step("Ввести Email {email}")
     public RegistrationPage setEmail(String email) {
         $("#userEmail").setValue(email);
         return this;
     }
 
+    @Step("Выбрать пол {gender}")
     public RegistrationPage selectGender(String gender) {
         $("[value='{SUBSTRING}']".replace("{SUBSTRING}", gender)).parent().click();
         return this;
     }
 
+    @Step("Ввести номер телефона {number}")
     public  RegistrationPage setNumberMobile(String number) {
         $("#userNumber").setValue(number);
         return this;
     }
 
+    @Step("Установить дату {day}.{month}.{year}")
     public RegistrationPage setDate(String day, String month, String year) {
         $("#dateOfBirthInput").click();
         calendarComponent.setMonth(month);
@@ -62,38 +67,45 @@ public class RegistrationPage {
         return this;
     }
 
+    @Step("Ввести в поле Subjects {subjects}")
     public RegistrationPage setSubjects(String subjects) {
         $("#subjectsContainer input").setValue(subjects).pressEnter();
         return this;
     }
 
+    @Step("Выбрать хобби {hobbies}")
     public RegistrationPage selectHobbies(String hobbies) {
         $(Selectors.byText(hobbies)).closest("div").click();
         return this;
     }
 
+    @Step("Загрузить фалй {path}")
     public RegistrationPage upLoadFile(String path) {
         $("#uploadPicture").uploadFromClasspath(path);
         return this;
     }
 
+    @Step("Ввести в поле Current Address {currentAddress}")
     public RegistrationPage setCurrentAddress(String currentAddress) {
         $("#currentAddress").setValue(currentAddress);
         return this;
     }
 
+    @Step("Выбрать State {state}")
     public RegistrationPage selectState(String state) {
         $("#state").click();
         $("#stateCity-wrapper").$(Selectors.byText(state)).click();
         return this;
     }
 
+    @Step("Выбрать City {city}")
     public RegistrationPage selectCity(String city) {
         $("#city").click();
         $("#stateCity-wrapper").$(Selectors.byText(city)).click();
         return this;
     }
 
+    @Step("Нажать кнопку Submit")
     public RegistrationPage clickSubmit() {
         $("#submit").click();
         return this;
--- src/test/java/pages/components/ModalComponent.java
+++ src/test/java/pages/components/ModalComponent.java
@@ -2,15 +2,18 @@ package pages.components;
 
 import com.codeborne.selenide.Condition;
 import com.codeborne.selenide.Selectors;
+import io.qameta.allure.Step;
 
 import static com.codeborne.selenide.Selenide.$;
 
 public class ModalComponent {
 
+    @Step("Проверить появление модального окна")
     public void verificationModal() {
         $(".modal-dialog.modal-lg").should(Condition.appear);
     }
 
+    @Step("Проверить, что в поле {key} введено {value}")
     public void verificationResult(String key, String value) {
         $(".table-responsive").$(Selectors.byText(key)).parent()
                 .shouldHave(Condition.text(value));
