bedfe85fcae3790212d4e4235fa7ef0d9d06f894
Alexandr Solovyov <18solovjov@gmail.com>
2023-06-11 17:15:16 +0300

    add annotations to a test checkStudentRegistrationForm()

--- src/test/java/RegistrationTests.java
+++ src/test/java/RegistrationTests.java
@@ -1,4 +1,6 @@
 import data.RegistrationTestsData;
+import io.qameta.allure.*;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.RepeatedTest;
 import org.junit.jupiter.api.Tag;
 import org.junit.jupiter.api.Test;
@@ -6,6 +8,12 @@ import pages.RegistrationPage;
 
 @Tag("UI")
 public class RegistrationTests extends BaseTest{
+    @Feature("Форма регистрации")
+    @Story("Заполнение формы регистрации")
+    @Owner("Александр Соловьев")
+    @DisplayName("Проверка заполения формы регистрации")
+    @Severity(SeverityLevel.BLOCKER)
+    @Link(value ="Form", url = "https://demoqa.com/automation-practice-form")
     @Test
     public void checkStudentRegistrationForm() {
         RegistrationTestsData data = new RegistrationTestsData();
