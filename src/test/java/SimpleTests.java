51e0eb1cf2cf706d0e2935ce90db57d7059b3bce
Alexandr Solovyov <18solovjov@gmail.com>
2023-06-13 23:22:47 +0300

    add SimpleTests

---
+++ src/test/java/SimpleTests.java
@@ -0,0 +1,13 @@
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class SimpleTests {
+    @Test
+    void check1() {
+        Assertions.assertTrue(true);
+    }
+    @Test
+    void check2() {
+        Assertions.assertTrue(true);
+    }
+}
